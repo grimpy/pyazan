b1ffa571db40df8ce0a044489feb91f789a42535
Jo De Boeck <deboeck.jo@gmail.com>
2010-04-10 00:23:11 +0200

    mend

--- pyazan/location.py
+++ pyazan/location.py
@@ -1,6 +1,35 @@
+import urllib, urllib2
+try:
+    import json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        import warnings
+        warnings.warn("Ussing dirty hack to load JSON please install simplejson")
+        class fakeJson(object):
+            def loads(self, str):
+                return eval(str)
+        json = fakeJson()
+
 class Location(object):
     def __init__(self, name=None, longitude=None, latitude=None, timezone=0):
         self.longitude = longitude
         self.latitude = latitude
         self.name = name
         self.timezone = timezone
+
+class GoogleLocation(object):
+    def __init__(self):
+        pass
+
+    def getUrl(self, search):
+        return "http://maps.google.com/maps/geo?%s" % urllib.urlencode({"q":search, "output":"json"})
+
+    def search(self, location):
+        urlo = urllib2.urlopen(self.getUrl(location))
+        data = urlo.read()
+        return json.loads(data)
+
+if __name__ == '__main__':
+    print GoogleLocation().search("Cairo")
--- pyazan/plugins/audiohandler/main.py
+++ pyazan/plugins/audiohandler/main.py
@@ -4,6 +4,7 @@ from pyazan.plugins import plugin
 
 class Plugin(plugin.Plugin):
     def __init__(self):
+        self.name = "audiohandler"
         self.file = None
         self.builder = None
 
@@ -21,30 +22,27 @@ class Plugin(plugin.Plugin):
         self.pyazangui.praynotifier.onTime.addCallback(self.play)
 
     def getAzanFile(self):
-        return self.pyazangui.options.getOption("audiohandler", "file", os.path.join(SOUND, "azan.mp3"))
+        return self.pyazangui.options.getOption(self.name, "file", os.path.join(SOUND, "azan.mp3"))
 
     def getVolume(self):
-        return int(self.pyazangui.options.getOption("audiohandler", "volume", 100))/100.0
+        return int(self.pyazangui.options.getOption(self.name, "volume", 100))/100.0
 
     def save(self):
         volume = int(self.builder.get_object("volume").get_value())
         self.volume = volume/100.0
-        self.pyazangui.options.setValue("audiohandler", "volume", volume)
+        self.pyazangui.options.setValue(self.name, "volume", volume)
         self.file = self.builder.get_object("file").get_filename()
-        self.pyazangui.options.setValue("audiohandler", "file", self.file)
+        self.pyazangui.options.setValue(self.name, "file", self.file)
 
     def unload(self):
         self.pyazangui.praynotifier.onTime.removeCallback(self.play)
 
     def getUiWidget(self):
-        import gtk
-        ui_config = os.path.join(XML, "audiohandler.xml")
-        self.builder = gtk.Builder()
-        self.builder.add_from_file(ui_config)
+        widget = super(Plugin, self).getUiWidget()
         self.builder.get_object("file").set_filename(self.getAzanFile())
         self.builder.get_object("volume").set_value(self.getVolume()*100)
         self.builder.get_object("btn_test").connect("released", self.play)
-        return self.builder.get_object("plugin")
+        return widget
 
     def getDescription(self):
         return "Play Athan sound!"
